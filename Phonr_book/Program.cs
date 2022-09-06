219578dc90012dae90aa9df1e6d88dcbf99b53d5
Андрей <nevraev1109@mail.ru>
2022-09-06 21:02:49 +0500

    Добавьте файлы проекта.

---
+++ Phonr_book/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Phonr_book
+{
+    internal class Program
+    {
+        class Phone
+        {
+            string Name;
+            long Number;
+            public void input()
+            {
+                Console.WriteLine("Имя ");
+                Name = Console.ReadLine();
+                Console.WriteLine("Номер");
+                Number = Convert.ToInt64(Console.ReadLine());   
+            }
+            public void Write()
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                path = path + @"\Phone.txt";
+                var a = new StreamWriter(path,true);
+                a.Write(Name + " ");
+                a.Write(Number + "\n");
+                a.Close();
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+           Phone phone = new Phone();
+            do
+            {
+                phone.input();
+                phone.Write();
+            } while (true);
+            
+         }
+    }
+}
